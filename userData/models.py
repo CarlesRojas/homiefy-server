dbfa57c5edb6779bf1d3b23733fdbe9574a32938
Jaume <jaume.gstocks@gmail.com>
2021-05-15 01:10:29 +0200

    Added Utilities Model

--- userData/models.py
+++ userData/models.py
@@ -3,3 +3,44 @@ from __future__ import unicode_literals
 from django.db import models
 from libs.dynamodb import dynamoDbInstance, dynamoDbTable
 # Create your models here.
+
+class UtilitiesEntry(dynamoDbInstance):
+    """
+        Table holding the Utilities of the house
+    """
+    VALID_CONSTRUCTOR_KEYS = ['username',
+                              'name',        #name of the utility
+                              'price',       #price of the utility
+                              'people',      #people that uses that utility
+                              'period',      #days of the utility period
+                              'lastPayment', #date of the last payment
+                              ]
+
+    def __init__(self, **kwargs):
+        self.constructor(**kwargs)
+        self.DYDB_TABLE = UtilitiesTable
+
+
+class UtilitiesTable(dynamoDbTable):
+    """
+        Table with the utilities
+    """
+
+    Name = 'Utilities'
+    DataInstanceFactory = UtilitiesEntry
+
+    AttributeDefinitions = [
+        {'AttributeName': 'name',          'AttributeType': 'S'},
+        {'AttributeName': 'username',      'AttributeType': 'S'},
+    ]
+
+
+    KeySchema = [
+        {'AttributeName': 'name',   'KeyType': 'HASH'},
+        {'AttributeName': 'username',  'KeyType': 'RANGE'},
+    ]
+
+    ProvisionedThroughput = {
+        'ReadCapacityUnits':  10,
+        'WriteCapacityUnits': 10
+    }
